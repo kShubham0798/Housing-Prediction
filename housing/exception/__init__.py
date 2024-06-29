7d34c1ff1b0975ba0fdff57ea9cf5a41b104fd47
Shubham Ranakoti <sranakoti30@gmail.com>
2024-06-29 19:58:03 +0530

    update

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -19,7 +19,7 @@ class HousingException(Exception):
         """
         _,_ ,exec_tb=error_detail.exc_info()
         file_name = exec_tb.tb_frame.f_code.co_filename
-        line_number=exec_tb.tb_frame.f_lineno
+        line_number=exec_tb.tb_lineno
         error_message=f"Error occured in script :[{file_name}] at line number : [{line_number}] error message: [{error_message}]"
 
         return error_message
